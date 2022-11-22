b00013082585633aa6777a9f123af7ce88aab461
alyssacgoins <alyssacg@bu.edu>
2022-11-22 10:54:00 -0500

    Alyssa First Commit

--- backend/src/main/java/com/_sb_backend/SpringBootCRUDRest/model/Model.java
+++ backend/src/main/java/com/_sb_backend/SpringBootCRUDRest/model/Model.java
@@ -1,5 +1,18 @@
 package com._sb_backend.SpringBootCRUDRest.model;
 
 public class Model {
-    
+        // endpoint --> soccer API
+        // 
+
+    // searching within exisiting database:
+        // (1) get
+            // takes in URL,, API keys
+
+    // user information:
+        // (1) put
+        // (2) post: ie. updating user informa
+        // (3) delete
+        // ie. brewery info, past games --> (going into a Mongo DB)
+
+    // Specific methods we need (from front-end people)
 }
